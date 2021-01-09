e6574cc22bb9622c6f6737bd6ffa846ef7369402
JBSilcott <silcott.jb@gmail.com>
2021-01-09 09:45:11 -0500

    update

--- PROJECTS/Angular/lawyer-app/src/app/app.module.ts
+++ PROJECTS/Angular/lawyer-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 //import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
